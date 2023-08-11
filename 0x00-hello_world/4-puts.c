cbacd4abd2047e0fc9bfae41d72fe716539801e6
Marie-rgb <akinyimarie3@gmail.com>
2023-08-11 00:54:58 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, followed by a new line."
- * return 0.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
-
+	return (0);
 }
-

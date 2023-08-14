b70eaa394d0578beb851311cddb59c4881f7aa77
Marie-rgb <akinyimarie3@gmail.com>
2023-08-14 01:20:59 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 	int num1, num2;
-
 	for (num1 = 0; num1 <= 98; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
@@ -18,7 +17,6 @@ int main(void)
 			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
-
 			if (num1 == 98 && num2 == 99)
 				continue;
 
@@ -26,8 +24,6 @@ int main(void)
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }

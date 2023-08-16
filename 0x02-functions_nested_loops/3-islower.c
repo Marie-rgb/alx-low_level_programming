78c8db59be366f245d539cf7322f94f0109334de
Marie-rgb <akinyimarie3@gmail.com>
2023-08-16 05:40:48 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print _islower(int c)
- * Return 1 if c is lowercase
+ * prints -  _islower(int c)
+ * Return 1 - if c is lowercase
  * Return 0 otherwise
  */
 
@@ -10,7 +10,7 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return 1;
+		return (1);
 	}
 	return (0);
 }

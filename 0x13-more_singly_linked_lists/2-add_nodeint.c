cda1e106047dc565b754603ecc55685adfbc211e
Marie-rgb <akinyimarie3@gmail.com>
2023-09-27 00:48:05 -0700

    singly linked list addition

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,29 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * listint_t - adds nodes to the beginning of a list
- *
- * @h: pointer of head of singly linked list
- * @n: number to add to the list
+ * add_nodeint - adds a new node at the beginning of a singly linked list
+ * @head: pointer to head of singly linked list
+ * @n: number to add as new node in the list
  *
  * Return: the address of the new element, or NULL if it failed
  */
-
-
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *n_node = malloc(sizeof(listint_t));
 
-	if(n_node == 0)
+	if (n_node == 0)
 		return (0);
-
 
 	n_node->n = n;
 	n_node->next = *head;
 	*head = n_node;
 
-	return(n_node);
+	return (n_node);
 }
 

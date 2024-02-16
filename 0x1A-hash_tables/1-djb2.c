e346b285a6a640844d291ba914fd561a57bece20
marie-rgb <akinyimarie3@gmail.com>
2024-02-15 23:21:28 -0800

    my commit

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,13 +1,18 @@
-#include "hash_tables.h"
-
 /**
- * key_index - gives the index of a key.
- * @key: the key, a string.
- * @size: the size of the array of the hash table.
+ * hash_djb2 - Hash function, takes a string and turns it into a hash number
+ * @str: the string, a constant value unrepeated in the hash table
  *
- * Return: Index number for the key/value pair in the hash table array.
+ * Return: The hash number
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+unsigned long int hash_djb2(const unsigned char *str)
 {
-	return (hash_djb2(key) % size);
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
